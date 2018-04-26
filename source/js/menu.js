c55b6da66a983490bcbe4e102cd9ddf8c4968097
Maxim_F <maxim.9408@mail.ru>
2018-04-26 17:56:10 +0300

    Header

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,14 @@
-var navMain = document.querySelector('.page-header');
-var navToggle = document.querySelector('.page-header__toggle');
-navMain.classList.remove('page-header--nojs');
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('page-header__toggle--closed')) {
-    navMain.classList.remove('page-header__toggle--closed');
-    navMain.classList.add('page-header__toggle--opened');
+var pHead = document.querySelector('.page-header');
+var menu_button = document.querySelector('.page-header__button');
+pHead.classList.remove('page-header--nojs');
+menu_button.addEventListener('click', function () {
+  if (pHead.classList.contains('page-header--closed')) {
+    pHead.classList.remove('page-header--closed');
+    menu_button.classList.remove('page-header__button--closed');
+    pHead.classList.add('page-header--opened');
   } else {
-    navMain.classList.remove('page-header__toggle--opened');
-    navMain.classList.add('page-header__toggle--closed');
+    pHead.classList.remove('page-header--opened');
+    menu_button.classList.add('page-header__button--closed');
+    pHead.classList.add('page-header--closed');
   }
 });
